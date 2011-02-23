9fb65e717b5ae3223eb05b49cb22c83ec3d37d3e
sam9414 <siau.jingwen.samantha@dhs.sg>
2011-02-23 09:40:24 +0800

    add hello.py and find_prime.py

---
+++ find_prime.py
@@ -0,0 +1,17 @@
+# find_prime.py
+# To find the nth prime number efficiently
+
+def is_prime():
+''' Function to determine if a number is prime
+    Input: integer k
+    Output: true if k is prime, else false
+'''
+
+# main
+# Get input n
+n = int(input("Enter n: "))
+
+# find the nth prime number
+
+# display
+print(ans)
